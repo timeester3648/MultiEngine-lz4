d917a5b203d80ffd64cc6164553c7deda9e76372
timeester3648 <weteringt@gmail.com>
2024-10-03 23:28:53 +0200

    Update

--- config/config.cpp
+++ config/config.cpp
@@ -1,13 +1,15 @@
 #include <MultiEngine/core/memory/MemoryAllocation.h>
 
-void* LZ4_malloc(size_t s) {
-	return MultiEngine::memory_allocate(s);
-}
+extern "C" {
+	void* LZ4_malloc(size_t s) {
+		return MultiEngine::memory_allocate(s);
+	}
 
-void* LZ4_calloc(size_t n, size_t s) {
-	return MultiEngine::memory_allocate_zero(n * s);
-}
+	void* LZ4_calloc(size_t n, size_t s) {
+		return MultiEngine::memory_allocate_zero(n * s);
+	}
 
-void  LZ4_free(void* p) {
-	MultiEngine::memory_free(p);
+	void  LZ4_free(void* p) {
+		MultiEngine::memory_free(p);
+	}
 }
